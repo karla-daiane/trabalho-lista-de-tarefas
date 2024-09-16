8baee1e361f30763e38450e0730ea446c7dd29b9
Karla Daiane <karladaiane.dev@gmail.com>
2024-09-16 16:08:37 -0300

    Adicionar interação com local storage e atualizar estilização

--- script.js
+++ script.js
@@ -3,21 +3,55 @@ const buttonNovaTarefa = document.querySelector('.button-nova-tarefa');
 const containerListaTarefas = document.querySelector('.container-lista-tarefas');
 const containerListaVazia = document.querySelector('.container-lista-vazia');
 
+buttonNovaTarefa.addEventListener('click', () => adicionarTarefa());
+
+inputNovaTarefa.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+        adicionarTarefa();
+    }
+});
+
+window.onload = () => carregarTarefas();
+
+const carregarTarefas = () => {
+    const tarefasArmazenadas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (tarefasArmazenadas.length > 0) {
+        containerListaVazia.classList.add('ocultar');
+        containerListaTarefas.classList.remove('ocultar');
+
+        for (const tarefa of tarefasArmazenadas) {
+            exibirTarefa(tarefa.description, tarefa.concluida);
+        }
+    }
+}
 
 const adicionarTarefa = () => {
     const inputValido = inputNovaTarefa.value.trim().length > 0;
+
     if (!inputValido) {
         return inputNovaTarefa.placeholder = "❗Insira uma tarefa";
     }
 
-    containerListaVazia.remove();
-    containerListaTarefas.classList.remove('ocultar');
+    exibirTarefa(inputNovaTarefa.value);
 
+    inputNovaTarefa.placeholder = "Adicionar nova tarefa";
+    inputNovaTarefa.value = "";
+
+    atualizarLocalStorage();
+}
+
+const exibirTarefa = (texto, estaConcluida) => {
+    
     const conteudoTarefa = document.createElement('div');
     conteudoTarefa.classList.add('tarefa');
 
     const textoTarefa = document.createElement('p');
-    textoTarefa.innerText = inputNovaTarefa.value;
+    textoTarefa.innerText = texto;
+
+    if (estaConcluida) {
+        textoTarefa.classList.add('concluido');
+    }
 
     textoTarefa.addEventListener('click', () => concluirTarefa(textoTarefa));
 
@@ -30,8 +64,9 @@ const adicionarTarefa = () => {
     conteudoTarefa.appendChild(textoTarefa);
     conteudoTarefa.appendChild(iconeDeletar);
     containerListaTarefas.appendChild(conteudoTarefa);
-    inputNovaTarefa.placeholder = "Adicionar nova tarefa";
-    inputNovaTarefa.value = "";
+    
+    containerListaVazia.classList.add('ocultar');
+    containerListaTarefas.classList.remove('ocultar');
 }
 
 const concluirTarefa = (textoTarefa) => {
@@ -42,6 +77,7 @@ const concluirTarefa = (textoTarefa) => {
             tarefa.firstChild.classList.toggle('concluido');
         }
     }
+    atualizarLocalStorage();
 }
 
 const deletarTarefa = (conteudoTarefa, textoTarefa) => {
@@ -51,6 +87,23 @@ const deletarTarefa = (conteudoTarefa, textoTarefa) => {
         if (tarefa.firstChild.isSameNode(textoTarefa))
             conteudoTarefa.remove();
     }
+    atualizarLocalStorage();
+
+    if (containerListaTarefas.children.length === 0) {
+        containerListaVazia.classList.remove('ocultar');
+        containerListaTarefas.classList.add('ocultar');
+    }
 }
 
-buttonNovaTarefa.addEventListener('click', () => adicionarTarefa());
+const atualizarLocalStorage = () => {
+    const tarefas = containerListaTarefas.childNodes;
+
+    const tarefasLocalStorage = [... tarefas].map(tarefa => {
+        const conteudoTarefa = tarefa.firstChild;
+        const concluida = conteudoTarefa.classList.contains('concluido');
+
+        return {description: conteudoTarefa.innerText, concluida};
+    })
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefasLocalStorage));
+};
